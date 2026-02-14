b090429c371204d4544ef7449144c9a1c109cbcb
GibGOAT <ceesaygibril40@gmail.com>
2026-02-14 16:29:16 -0500

    LEAAAArend cooooooooodign
    yyyeyeyyeyeyyyyyeyygtggd

---
+++ Assets/geoquest/scenes/Geocontroller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Geocontroller : MonoBehaviour
+{
+    string chat = "hello";
+    int chat4 = 67676;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Heeyyyyyyy");
+        string chat3 = "hello";
+        Debug.Log(chat + chat3 + chat4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
