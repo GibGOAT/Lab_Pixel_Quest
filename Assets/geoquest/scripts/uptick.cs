3bf051c9d0e5b9610843547c1b6d7dd5d7ca1d81
GibGOAT <ceesaygibril40@gmail.com>
2026-03-07 13:36:51 -0500

    moving but getkeydown

--- Assets/geoquest/scripts/uptick.cs
+++ Assets/geoquest/scripts/uptick.cs
@@ -16,7 +16,20 @@ public class uptick : MonoBehaviour
     {
 
         if (Input.GetKeyDown(KeyCode.W))
-        {transform.position+= new Vector3(0,1,0);
+        {
+            transform.position+= new Vector3(0,1,0);
             }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position-=new Vector3(1,0,0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position -= new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
     }
 }
