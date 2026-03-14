65c984938b4ae65e3058ea8e240389e61c0fb7b9
GibGOAT <ceesaygibril40@gmail.com>
2026-03-14 16:25:07 -0400

    I WENT ONNA GEOOOOOQUEST

--- Assets/geoquest/scripts/uptick.cs
+++ Assets/geoquest/scripts/uptick.cs
@@ -6,78 +6,51 @@ using UnityEngine.SceneManagement;
 public class uptick : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public int speed = 5;
-    public string nextLevel = "brrskibidi 2";
+    public int speed = 4;
+    public string nextLevel = "Level2";
     private SpriteRenderer sr;
 
-    // Start is called before the first frame update
+    private float xInput;
+    private float yInput;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         switch (collision.tag)
         {
             case "Death":
-                {
-                    string thislevel = SceneManager.GetActiveScene().name;
-                    SceneManager.LoadScene(thislevel);
-                    break;
-                }
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                break;
             case "Finish":
-                {
-                    SceneManager.LoadScene(nextLevel);
-                    break;
-                }
-
-
+                SceneManager.LoadScene(nextLevel);
+                break;
         }
-
     }
-    // Update is called once per frame
+
     void Update()
     {
-        float xInput = Input.GetAxis("Horizontal");
+        // Read player input
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical"); // optional for buoyancy-assisted movement
 
-        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
-        if (Input.GetKeyDown(KeyCode.Alpha0))
-        {
-            sr.color = Color.white;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            sr.color = Color.red;
-        }
+        // Color changing
+        if (Input.GetKeyDown(KeyCode.Alpha0)) sr.color = Color.white;
+        if (Input.GetKeyDown(KeyCode.Alpha1)) sr.color = Color.red;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) sr.color = Color.green;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) sr.color = Color.blue;
+    }
 
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            sr.color = Color.green;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            sr.color = Color.blue;
-        }
-        /*
-                if (Input.GetKeyDown(KeyCode.W))
-                {
-                    transform.position+= new Vector3(0,1,0);
-                    }
-                if (Input.GetKeyDown(KeyCode.A))
-                {
-                    transform.position-=new Vector3(1,0,0);
-                }
-                if (Input.GetKeyDown(KeyCode.S))
-                {
-                    transform.position -= new Vector3(0,1,0);
-                }
-                if (Input.GetKeyDown(KeyCode.D))
-                {
-                    transform.position += new Vector3(1,0,0);
-                }
-        */
-
+    void FixedUpdate()
+    {
+        // Apply movement using Rigidbody2D – keeps effectors working
+        Vector2 velocity = rb.velocity;
+        velocity.x = xInput * speed;
+        velocity.y = rb.velocity.y; // Keep vertical velocity for Buoyancy Effector
+        rb.velocity = velocity;
     }
 }
